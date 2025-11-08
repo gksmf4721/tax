d8002c122d750a27c9cdc90f8334bd17c15ed568
dev_n <gksmf4721@naver.com>
2025-11-08 18:37:03 +0900

    init: 부가세 프로젝트 생성

---
+++ src/test/kotlin/com/kcd/tax/TaxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kcd.tax
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
